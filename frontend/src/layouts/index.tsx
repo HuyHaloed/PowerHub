f3f8489279a45e49f1abe4577202b5b29c7e5a4e
Đoàn Trí Hùng <koikoidth12@gmail.com>
2025-03-22 11:03:54 +0700

    feat/set up skeleton fe code

---
+++ frontend/src/layouts/index.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "./AuthLayout";
+import PublicLayout from "./PublicLayout";
+import ProtectedLayout from "./ProtectedLayout";
+import AdminLayout from "./AdminLayout";
+
+export const Layouts = {
+  AuthLayout,
+  PublicLayout,
+  ProtectedLayout,
+  AdminLayout,
+};
---
+++ frontend/src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/styles/index.css";
+import App from "./App.tsx";
+import "react-toastify/dist/ReactToastify.css";
+import "nprogress/nprogress.css";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { ReactQueryClientProvider } from "./hooks/queryProvider.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+);
