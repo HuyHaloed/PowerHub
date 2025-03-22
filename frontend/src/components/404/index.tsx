29a7607b52bd2d01dc27fab855590faab530afc0
Đoàn Trí Hùng <koikoidth12@gmail.com>
2025-03-22 15:15:57 +0700

    set up home page

--- frontend/src/components/404/index.tsx
+++ frontend/src/components/404/index.tsx
@@ -1,10 +1,8 @@
 import { Button } from "../ui/button";
-import { ModeToggle } from "@/components/mode-toggle";
 export default function NotFoundPage() {
   return (
     <div>
       <h1 className="text-primary">404 page</h1>
-      <ModeToggle />
       <Button variant={"secondary"}>Haha</Button>
     </div>
   );
--- frontend/src/layouts/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout.tsx
@@ -1,10 +1,11 @@
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 export default function PublicLayout() {
   return (
-    <div>
-      <h1>Public page</h1>
-      <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <Header/>
+      <Outlet/>
     </div>
   );
 }
